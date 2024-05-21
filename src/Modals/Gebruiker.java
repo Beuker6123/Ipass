1c254638da8b1592d655c13bdaa701f30790908f
Beuker6123 <jayaltevogt1@gmail.com>
2024-05-21 15:27:02 +0200

    alle klasse zijn aangemaakt ik ga in de les morgen vragen waarom ik ze niet als normale java bestanden kan bekijken

---
+++ src/Modals/Gebruiker.java
@@ -0,0 +1,12 @@
+
+
+public class gebruiker {
+    private String naam, achternaam, adres, postcode, email;
+    private int id, telnr;
+    private enum role;
+
+    public gebruiker (nm, Anm, adr, pc, em, telnr, role){
+        naam = nm;
+
+    }
+}
