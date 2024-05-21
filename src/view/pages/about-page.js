06ed0e39e80f17453bc17084bda37040f4e6b1a2
Beuker6123 <jayaltevogt1@gmail.com>
2024-05-21 14:24:43 +0200

    setup ipass_2

---
+++ src/view/pages/about-page.js
@@ -0,0 +1,7 @@
+import { CurrentDate } from '../components/current-date';
+const dateElement = document.querySelectorAll('.current-date') ||
+    [];
+dateElement.forEach((element) => {
+    // eslint-disable-next-line no-new
+    new CurrentDate(element);
+});
