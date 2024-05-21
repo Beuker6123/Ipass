9f46831b8f2d243dc1892fae3c5f782de91f6cc2
Beuker6123 <jayaltevogt1@gmail.com>
2024-05-21 14:31:54 +0200

    New build for deploy: Tue, May 21, 2024  2:31:52 PM

---
+++ assets/about-Bc_58zaC.js
@@ -0,0 +1,2 @@
+import{C as t}from"./current-date-Cq2oIrHL.js";const r=document.querySelectorAll(".current-date")||[];r.forEach(e=>{new t(e)});
+//# sourceMappingURL=about-Bc_58zaC.js.map
---
+++ assets/main-D6rLhkhj.js
@@ -0,0 +1,2 @@
+import{C as t}from"./current-date-Cq2oIrHL.js";const r=document.querySelectorAll(".current-date")||[];r.forEach(e=>{new t(e)});
+//# sourceMappingURL=main-D6rLhkhj.js.map
